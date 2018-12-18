4bf2cf58f3d2fbb3ba0c71a3e2b1bdc818224179
Brian Canela <briancanela@Brians-iMac.socal.rr.com>
2018-12-18 00:28:46 -0800

    Created the Managed object model, and also saving the data to core data

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var names: [String] = []
+    var people: [NSManagedObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +34,7 @@ class ViewController: UIViewController {
                 return
             }
             
-            self.names.append(nameToSave)
+            self.save(name: nameToSave)
             self.tableView.reloadData()
         }
         
@@ -47,6 +48,35 @@ class ViewController: UIViewController {
     }
     
     
+    func save(name:String){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else{
+             return
+        }
+        
+        //GET REFERENCE TO APP DELEGATE
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        //CREATE A NEW MANAGED OBJECT AND INSERT IT INTO THE MANAGED OBJECT CONTEXT
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+        
+        let person = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        //SET THE NAME ATTRIBUTE
+        person.setValue(name, forKey: "name")
+        
+        //COMMIT CHANGES TO PERSON AND SAVE TO DISK
+        do {
+            try managedContext.save()
+            people.append(person)
+        }catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+        
+        
+        
+    }
+    
+    
 }
 
 
@@ -54,15 +84,17 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return people.count
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        let person = people[indexPath.row]
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        cell.textLabel?.text = names[indexPath.row]
+        cell.textLabel?.text = person.value(forKeyPath: "name") as? String   //names[indexPath.row]
         
         return cell
     }
