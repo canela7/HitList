6d455991bfa6eb14702f7e6660f0cbe98cd1d600
Brian Canela <briancanela@brians-imac.local>
2018-12-17 16:57:13 -0800

    Initial Commit

---
+++ HitList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Brian Canela on 12/17/18.
+//  Copyright © 2018 Brian Canela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
